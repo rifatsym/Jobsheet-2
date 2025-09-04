88c6c415f60a1ee29b86195841b11995831eba66
rifat syamlan <rifatsyamlan78@gmail.com>
2025-09-04 21:53:13 +0700

    first commit

---
+++ MahasiswaDemo.java
@@ -0,0 +1,12 @@
+public class MahasiswaDemo {
+
+    public static void main(String[] args) {
+        mahasiswa m1 = new mahasiswa();
+        m1.nim = "023432";
+        m1.nama = "Yansy Ayuningtyas";
+        m1.alamat = "Nias, Sumatera Utara";
+        m1.kelas = "2A";
+
+        m1.displayBiodata();
+    }
+}
---
+++ mahasiswa.java
@@ -0,0 +1,13 @@
+public class mahasiswa {
+    public String nama;
+    public String nim;
+    public String alamat;
+    public String kelas;
+
+    public void displayBiodata() {
+        System.out.println("NIM: " + nim);
+        System.out.println("nama: " + nama);
+        System.out.println("alamat: " + alamat);
+        System.out.println("kelas: " + kelas);
+    }
+}
