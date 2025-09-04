698981f9be7b382bf4e6f5f4750b17ac9062ca02
rifat syamlan <rifatsyamlan78@gmail.com>
2025-09-04 23:47:08 +0700

    commit

---
+++ PersegiDemo.java
@@ -0,0 +1,9 @@
+public class PersegiDemo {
+    public static void main(String[] args) {
+        PersegiPanjang p1 = new PersegiPanjang();
+        p1.panjang = 10;
+        p1.lebar = 5;
+
+        p1.displayInfo();
+    }
+}
