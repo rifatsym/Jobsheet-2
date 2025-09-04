5b242268d1e2022af6b33e5aae273af41679c728
rifat syamlan <rifatsyamlan78@gmail.com>
2025-09-04 22:29:33 +0700

    commit

--- MahasiswaDemo.java
+++ MahasiswaDemo.java
@@ -7,6 +7,20 @@ public class MahasiswaDemo {
         m1.alamat = "Nias, Sumatera Utara";
         m1.kelas = "2A";
 
+        mahasiswa m2 = new mahasiswa();
+        m2.nim = "123456";
+        m2.nama = "Budi Santoso";
+        m2.alamat = "Jakarta, Indonesia";
+        m2.kelas = "2D";
+
+        mahasiswa m3 = new mahasiswa();
+        m3.nim = "654321";
+        m3.nama = "Siti Aminah";
+        m3.alamat = "Bandung, Jawa Barat";
+        m3.kelas = "2B";
+
         m1.displayBiodata();
+        m2.displayBiodata();
+        m3.displayBiodata();
     }
 }
